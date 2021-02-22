800496b44745651e7b882b776ba6053af8003ba4
coolcard <gaoz@sina.com>
2021-02-22 21:29:50 +0800

    点击 Logo 刷新页面

--- src/components/tree/TreeLogo.vue
+++ src/components/tree/TreeLogo.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="d-flex pa-2 align-center">
+  <div role="button" class="d-flex pa-2 align-center" @click="refresh">
     <img src="/images/v-logo-small.png" height="30" alt="logo" class="mr-1">
     <div>
       <div class="text-h6 primary--text">宇航产品数据中心</div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    // 点击 Logo 刷新页面
+    refresh() {
+      location.reload()
+    }
+  }
+}
+</script>
